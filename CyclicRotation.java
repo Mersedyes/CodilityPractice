3d6d853add9d9acf5702f5f554f2558db6c020e6
Mersedyes Livingston <98776643+Mersedyes@users.noreply.github.com>
2023-01-06 17:04:37 -0600

    Added class
    Setup comments and initial potential solution for cyclicrotation class

---
+++ CyclicRotation.java
@@ -0,0 +1,29 @@
+/* The APPROACH
+* ARRAY A CANNOT BE NULL AND HAS TO HAVE AT LEAST ONE ELEMENT
+* IF K IS 0 OR THE LENGTH OF THE ARRAY IS ONLY 1, RETURN THE ARRAY
+* CALCULATE THE ACTUAL NUMBER OF ROTATIONS TO PERFORM
+* ROTATE THE ARRAY
+* RETURN THE ROTATED ARRAY
+*
+* CONSTRAINTS
+* N AND K ARE INTEGERS WITHIN 0-100
+* EACH ELEMENT OF THE ARRAY A IS AN INTEGER WITHIN THE RANGE -1000 TO 1000*/
+
+import static java.util.Collections.reverse;
+
+public class CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        if (A == null || A.length == 0 || K == 0 || A.length == 1){
+            return A;
+        } else if (K < 0 || K > 100){
+            throw new IllegalArgumentException("K must be an integer between 0 and 100");
+        } else {
+            K = K % A.length;
+            if (K < 0){
+                K += A.length;
+            }
+        }
+        return A;
+    }
+
+}
